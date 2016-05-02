318eae3a4ec9072dc0b68f2d2a400012740e7b88
milesfw <https://github.com/ChiefSH>
2016-05-02 16:37:01 -0400

    End of Day 1... Began setting up Models and Views

---
+++ DS3_FightclubBlacklist/Models/BLContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DS3_FightclubBlacklist.Models
+{
+    public class BLContext
+    {
+        public string Location { get; set; }
+        
+        public string Summary { get; set; }
+
+        public string Context { get; set; }
+    }
+}
---
+++ DS3_FightclubBlacklist/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DS3_FightclubBlacklist.Models
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public string Location { get; set; }
+        public DateTime EncounterTime { get; set; }
+
+        //Need to integrate Collections here
+    }
+}
